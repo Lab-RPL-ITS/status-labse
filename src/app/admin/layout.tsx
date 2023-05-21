0566be062e970856e3237afe9a21c952336c99a6
dikiogres <dikiogres@gmail.com>
2023-05-21 22:53:25 +0700

    feat: add dashboard layout

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Admin | RPL',
+  description: 'Check Status Lab RPL',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+        <html lang="en">
+          <body className={inter.className}>
+            <a href="/">Back to home</a>
+            {children}
+            </body>
+        </html>
+    </>
+  )
+}
